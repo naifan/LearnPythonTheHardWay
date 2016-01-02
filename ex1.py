cbccfdb23b35a6aee821b380f28f870ec244386d
naifan <haokqgood@gmail.com>
2016-01-02 20:34:45 +0800

    Practice Everyday!

---
+++ ex1.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
---
+++ ex2.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+print "Hi # there!"
+# this is comment
